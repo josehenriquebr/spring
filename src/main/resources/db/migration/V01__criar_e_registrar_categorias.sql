c332f7be3f06f7c36ee288f0e477328517f96c2e
jose <josehenriquebr@gmail.com>
2020-07-17 12:02:14 -0300

    subindo arquivos do projeto spring

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,12 @@
+create table categoria (
+	codigo bigint(20) primary key auto_increment,
+	nome varchar(50) not null
+) engine=InnoDB Default charset=utf8;
+
+insert into categoria(nome) values('Lazer');
+insert into categoria(nome) values('Alimentação');
+insert into categoria(nome) values('Supermercado');
+insert into categoria(nome) values('Farmácia');
+insert into categoria(nome) values('Outros');
+
+
