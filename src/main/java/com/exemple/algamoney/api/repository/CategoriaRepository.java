ddf05612056c075162a610fb67ba23e1239ceed0
jose <josehenriquebr@gmail.com>
2020-07-17 16:02:53 -0300

    iniciando as classes

---
+++ src/main/java/com/exemple/algamoney/api/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.exemple.algamoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.exemple.algamoney.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+
+}
