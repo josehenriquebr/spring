bb83a297fcc29b06061a348b3cdb118714c10d60
jose <josehenriquebr@gmail.com>
2020-07-31 15:03:43 -0300

    gravando novas categorias metodo Criar

--- src/main/java/com/exemple/algamoney/api/repository/CategoriaRepository.java
+++ src/main/java/com/exemple/algamoney/api/repository/CategoriaRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.exemple.algamoney.api.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-	
 
 }
--- src/main/java/com/exemple/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/exemple/algamoney/api/resource/CategoriaResource.java
@@ -1,10 +1,59 @@
 package com.exemple.algamoney.api.resource;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.http.HttpHeaders;
+
+import com.exemple.algamoney.api.model.Categoria;
+import com.exemple.algamoney.api.repository.CategoriaRepository;
+
+import reactor.netty.http.server.HttpServerResponse;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public ResponseEntity<?> listar(){
+		List<Categoria> categorias = categoriaRepository.findAll(); 
+		
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
+		
+	}
 
+	@PostMapping
+//	@ModelAttribute
+//	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		
+		System.out.println(uri.toASCIIString());
+		
+		response.addHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);		
+		
+	}
 }
